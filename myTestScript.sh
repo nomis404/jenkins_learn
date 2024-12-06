f568de84ec2dcbecc05357dd7da5ec2b83e6047c
nomis404 <64785573+nomis404@users.noreply.github.com>
2024-12-06 18:59:19 +0530

    Update myTestScript.sh
    Testing Mail function

--- myTestScript.sh
+++ myTestScript.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir /home/dedsec/jenkins_test
-touch /home/dedsec/jenkins_test/hello.txt
+toch /home/dedsec/jenkins_test/hello.txt
 echo "Hey " >> /home/dedsec/jenkins_test/hello.txt
