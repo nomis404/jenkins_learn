cb06789e0da6e96b38349ad2b529e9c71b316904
nomis404 <64785573+nomis404@users.noreply.github.com>
2024-12-06 18:48:55 +0530

    Update myTestScript.sh

--- myTestScript.sh
+++ myTestScript.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir /home/dedsec/jenkins_test
 touch /home/dedsec/jenkins_test/hello.txt
-echo "Hello " >> /home/dedsec/jenkins_test/hello.txt
+echo "Hey " >> /home/dedsec/jenkins_test/hello.txt
