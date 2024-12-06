4352a0ea1448290916c3d181eaacf68724e8fe3c
nomis404 <64785573+nomis404@users.noreply.github.com>
2024-12-06 18:40:32 +0530

    Update myTestScript.sh
    Updated the corrections in line number 2

--- myTestScript.sh
+++ myTestScript.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir /home/dedsec/jenkins_test
-touch /home/dedsec/jenkins_testhello.txt
+touch /home/dedsec/jenkins_test/hello.txt
 echo "Hello World" >> /home/dedsec/jenkins_test/hello.txt
