240465f57b3db6cfa45913bd59a87cadd00ff46b
nomis404 <64785573+nomis404@users.noreply.github.com>
2024-12-07 11:19:55 +0530

    Update myTestScript.sh
    Updating script for Remote server

--- myTestScript.sh
+++ myTestScript.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-mkdir /home/dedsec/jenkins_test
-touch /home/dedsec/jenkins_test/hello.txt
-echo "Mail test " >> /home/dedsec/jenkins_test/hello.txt
+mkdir jenkins_test
+touch jenkins_test/hello.txt
+echo "Mail test " >> hello.txt
